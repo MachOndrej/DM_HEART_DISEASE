d84084d25bd7700e7e196b9205217ef830d03080
MachOndrej <ondrej.mach3@tul.cz>
2023-05-14 19:34:19 +0200

    Odstranění nepotřebných

--- try_me.R
+++
@@ -1,7 +0,0 @@
-heart_dis <- read.csv("HEART2.csv", header=TRUE, sep="\t")
-
-# This section of code covers data preprocessing.  It includes exploration of 
-# the original dataset, removing variables, and dealing with missing values.
-
-# Previews the heart disease dataset.
-View(heart_dis)
